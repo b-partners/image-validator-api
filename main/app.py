aee4161667cd9389aa2b09004d5415515a333cda
Dinasoa <hei.dinasoa@gmail.com>
2026-05-04 16:48:58 +0300

    fix: improve OCR reliability for failed text detection

--- main/app.py
+++ main/app.py
@@ -1,6 +1,8 @@
 import base64
 import io
 import json
+
+import cv2
 import pytesseract
 import numpy as np
 from PIL import Image
@@ -35,23 +37,40 @@ def is_image_corrupted(base64_img):
         return False
 
     except Exception as e:
-        # If any exception occurs during decoding/processing, image is likely corrupted
+        # If any exception occurs during decoding/processing, image is likely corrupted.jpeg
         print(f"Image corruption detected: {str(e)}")
         return True
 
-
 def image_contains_failed_text(img):
-    """
-    Retourne True si le texte OCRisé de l'image contient l'un des mots-clés indiquant un échec.
-    """
+    """Retourne True si l'image contient un mot-clé d'échec."""
     failed_keywords = ("failed", "wmts")
-    gray = img.convert("L")
+
+    gray = np.array(img.convert("L"))
+
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
     custom_config = r'--oem 3 --psm 6'
-    try_text = pytesseract.image_to_string(gray, config=custom_config)
-    txt_norm = try_text.lower()
+    try_text = pytesseract.image_to_string(thresh, config=custom_config)
+
+    txt_norm = try_text.lower().strip()
+    # print(f"[DEBUG] OCR result: '{txt_norm}'")
+
     return any(k in txt_norm for k in failed_keywords)
 
 def is_img_blank(img):
     if np.all(img == 0) or np.all(img == 255):
         return True
     return False
+
+if __name__ == "__main__":
+    with open("saint-denis.jpeg", "rb") as f:
+        img_base64 = base64.b64encode(f.read()).decode("utf-8")
+
+    event = {
+        "body": json.dumps({
+            "base64image": img_base64
+        })
+    }
+
+    result = lambda_handler(event, None)
+    print(result)
